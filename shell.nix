85160c604c30c95117cb5b5070be8eee926706f5
Adrian Jastrzębski <business@quantdevlabs.com>
2025-11-25 16:21:55 +0100

    Update shell.nix to correctly support rust-analyzer on NixOS VSC

--- shell.nix
+++ shell.nix
@@ -1,15 +1,25 @@
-{ pkgs ? (import <nixpkgs> {}).pkgs }:
-with pkgs;
-mkShell {
-  buildInputs = [
-      rustc
-      rustfmt
-      cargo
-      cargo-info
-      clippy
-      rust-analyzer-unwrapped
-      openssl
-      pkg-config
+let
+  rustOverlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pkgs = import <nixpkgs> { overlays = [ (import rustOverlay) ]; };
+in
+
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [ pkg-config ];
+  buildInputs       = with pkgs; [ openssl ];
+
+  packages = with pkgs; [
+    rust-bin.stable.latest.default           # rustc + cargo
+    rust-bin.stable.latest.rust-analyzer
+    rust-bin.stable.latest.rust-src
+    cargo-info
   ];
-    PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig";  
+
+  RUST_SRC_PATH   = "${pkgs.rust-bin.stable.latest.rust-src}/lib/rustlib/src/rust/library";
+  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+
+  shellHook = ''
+    echo "Rust dev shell ready"
+    echo " rustc $(rustc --version)"
+    echo " rust-analyzer $(rust-analyzer --version)"
+  '';
 }
